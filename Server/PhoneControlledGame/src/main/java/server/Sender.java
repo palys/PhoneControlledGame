9c0c446e51df5ad65e3fcac7555a16fc03aa1d53
Tomek <tomek201992@gmail.com>
2015-09-15 23:46:25 +0200

    Add manifest and inputServer skeleton.

---
+++ Server/PhoneControlledGame/src/main/java/server/Sender.java
@@ -0,0 +1,30 @@
+package server;
+
+import game.Game;
+import game.InputServer;
+import game.Message;
+import game.Player;
+
+public class Sender implements InputServer {
+
+	public void notifyPlayers(Message message) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void onGameEnded(Player winner, Message message) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void registerGame(Game game) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void sendMesage(Player to, Message message) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
